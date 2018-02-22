d9ede67112f224ddae849d7de832ecd2f7c2b17a
connorkoch <connorkoch@mymail.mines.edu>
2018-02-22 16:37:06 -0700

    Added comment 'sup'

--- src/Cat.java
+++ src/Cat.java
@@ -31,6 +31,7 @@ public class Cat {
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.green);
+		//sup 
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
